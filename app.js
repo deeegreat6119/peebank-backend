9f3bf17e6e3b9ac601b765f002ade56595c22eeb
anthony peter <www.anthonygoldpeter@gmail.com>
2025-04-28 04:07:37 -0700

    push

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.FRONTEND_BASE,
   methods: ['GET', 'POST', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
